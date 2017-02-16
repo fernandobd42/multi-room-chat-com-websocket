1bcf10c27652d84c17ae3ab597f337c80d8df816
fernandobd43 <fernandolucasbd@gmail.com>
2017-02-16 11:31:26 -0500

    validando formulario de entrada com express-validator

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,4 +1,16 @@
 module.exports.iniciaChat = function(application, req, res){
 
+    var dadosForm = req.body;
+
+    req.assert('nome', 'Nome é obrigatório').notEmpty();
+    req.assert('nome', 'Nome deve conter entre 3 e 20 caracteres').len(3,15);
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.render("index", {validacao: erros});
+        return;
+    }
+
     res.render("chat");
 }
