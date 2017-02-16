341111aaa4663abd017754861479f287427b34da
fernandobd43 <fernandolucasbd@gmail.com>
2017-02-16 10:59:17 -0500

    ajustando as routes e views

--- config/server.js
+++ config/server.js
@@ -14,11 +14,11 @@ var expressValidator = require('express-validator');
 var app = express();
 
 // setar as variáveis 'view-engine' e 'views' do express
-app.set('view-engine', 'ejs');
-app.set('view', './app/views');
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
 
 // configurar o middleware express.static
-app.use(express.static('./public'));
+app.use(express.static('./app/public'));
 
 // configurar o middleware body-parser
 app.use(bodyParser.urlencoded({extended: true}));
