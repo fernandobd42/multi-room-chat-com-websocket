e3cfe3336388c90cb8f797d0b012e98acaf9dff5
fernandobd43 <fernandolucasbd@gmail.com>
2017-02-17 07:54:38 -0500

    integrando o socket.io ao servidor

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // importa as configurações do servidor
 var app = require('./config/server');
 
-// parametrizar a porta que irá receber requisições
-app.listen(80, function(){
+// parametriza a porta que irá receber requisições
+var server = app.listen(80, function(){
     console.log('Servidor ON');
 });
+
+// integra socket.io ao servidor 
+require('socket.io').listen(server);
